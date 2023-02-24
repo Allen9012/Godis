1b77380de803479c18bad7f98f981fa753fd51d5
Allen <2439506288@qq.com>
2023-02-24 20:06:57 +0800

    Redis协议解释器 v0.0.6 实现Parser

--- resp/parser/parser.go
+++ resp/parser/parser.go
@@ -2,15 +2,21 @@ package parser
 
 import (
 	"Gedis/interface/resp"
+	"Gedis/lib/logger"
 	"Gedis/resp/reply"
 	"bufio"
 	"errors"
 	"io"
+	"runtime/debug"
 	"strconv"
 	"strings"
 )
 
 // PayLoad 客户端发送的数据
+//
+//  PayLoad
+//  @Description: 有数据就写到数据字段，反之则写到错误字段
+//
 type PayLoad struct {
 	// 服务端和客户交互的内容实际是类似的，都是reply
 	Data resp.Reply
@@ -35,13 +41,110 @@ func (s *readState) finished() bool {
 // ParseStream 实际可以调用的支持并发的异步的Parser 返回一个只发送的管道
 func ParseStream(reader io.Reader) <-chan *PayLoad {
 	ch := make(chan *PayLoad)
-	go parse0(reader, ch)
-	return ch
+	go parse0(reader, ch) //	一个用户一个解析器
+	return ch             //	返回一个channel，redis核心一直监听ch看看有没有数据产生
 }
 
 // parse0 解析器核心
 func parse0(reader io.Reader, ch chan<- *PayLoad) {
-
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error(string(debug.Stack()))
+		}
+	}()
+	bufReader := bufio.NewReader(reader)
+	var state readState
+	var err error
+	var msg []byte
+	// 循环解析
+	for true {
+		var ioErr bool
+		msg, ioErr, err = readLine(bufReader, &state)
+		if err != nil {
+			if ioErr {
+				ch <- &PayLoad{
+					Err: err,
+				}
+				close(ch)
+				return
+			}
+			ch <- &PayLoad{
+				Err: err,
+			}
+			state = readState{}
+			continue
+		}
+		// 判断多行还是单行模式和
+		if !state.readingMultiLine {
+			if msg[0] == '*' { //eg:*3
+				// 利用下面的方法会解析出3，然后修改readingMultiLine改成多行模式
+				err = parseMultiBulkHeader(msg, &state)
+				if err != nil {
+					ch <- &PayLoad{
+						Err: err,
+					}
+					state = readState{}
+					continue
+				}
+				if state.expectedArgsCount == 0 {
+					ch <- &PayLoad{
+						Data: &reply.EmptyMultiBulkReply{},
+					}
+					state = readState{}
+					continue
+				}
+			} else if msg[0] == '$' { // 一开始就遇到 $3\r\n
+				err = parseBulkHeader(msg, &state)
+				if err != nil {
+					ch <- &PayLoad{
+						Err: errors.New("protocol error: " + string(msg)),
+					}
+					state = readState{}
+					continue
+				}
+				// 空指令
+				if state.bulkLen == -1 { // $-1\r\n
+					ch <- &PayLoad{
+						Data: &reply.EmptyMultiBulkReply{},
+					}
+					state = readState{}
+					continue
+				}
+			} else {
+				result, err := parseSingleLineReply(msg)
+				if err != nil {
+					ch <- &PayLoad{
+						Data: result,
+						Err:  err,
+					}
+					state = readState{}
+					continue
+				}
+			}
+		} else { // 读多行
+			err := readBody(msg, &state)
+			if err != nil {
+				ch <- &PayLoad{
+					Err: errors.New("protocol error: " + string(msg)),
+				}
+				state = readState{}
+				continue
+			}
+			if state.finished() {
+				var result resp.Reply
+				if state.msgType == '*' {
+					result = reply.MakeMultiBulkReply(state.args)
+				} else if state.msgType == '$' {
+					result = reply.MakeBulkReply(state.args[0])
+				}
+				ch <- &PayLoad{
+					Data: result,
+					Err:  err,
+				}
+				state = readState{}
+			}
+		}
+	}
 }
 
 //
@@ -86,7 +189,7 @@ func readLine(bufReader *bufio.Reader, state *readState) ([]byte, bool, error) {
 
 //
 // parseMultiBulkHeader
-//  @Description: readLine之后具体取出响应内容
+//  @Description: readLine之后具体取出响应内容，解析多行响应内容
 //  @param msg
 //  @param state
 //  @return error	填充state
@@ -107,6 +210,32 @@ func parseMultiBulkHeader(msg []byte, state *readState) error {
 		state.readingMultiLine = true
 		state.expectedArgsCount = int(expectedLine)
 		state.args = make([][]byte, 0, expectedLine)
+		return nil
+	} else {
+		return errors.New("protocol error: " + string(msg))
+	}
+}
+
+//
+// parseBulkHeader
+//  @Description: 解析单行数据
+//  @param msg
+//  @param state
+//  @return error
+//
+func parseBulkHeader(msg []byte, state *readState) error {
+	var err error
+	state.bulkLen, err = strconv.ParseInt(string(msg[1:len(msg)-2]), 10, 64)
+	if err != nil {
+		return errors.New("protocol error: " + string(msg))
+	}
+	if state.bulkLen == -1 { // null bulk
+		return nil
+	} else if state.bulkLen > 0 {
+		state.msgType = msg[0]
+		state.readingMultiLine = true
+		state.expectedArgsCount = 1
+		state.args = make([][]byte, 0, 1)
 		return nil
 	} else {
 		return errors.New("protocol error: " + string(msg))
